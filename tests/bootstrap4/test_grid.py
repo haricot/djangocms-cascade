6a677db4ad34c0df1f7536847bf8682ec009715e
Nicolas PASCAL <np.pascal@gmail.com>
2018-07-01 02:36:47 +0200

    Fix test  col-auto : the missed gutter in bound.max and rounded

--- tests/bootstrap4/test_grid.py
+++ tests/bootstrap4/test_grid.py
@@ -28,6 +28,7 @@ def test_xs_cols_with_flex():
     assert row[1].get_bound(Breakpoint.md) == Bound(270.0, 270.0)
     assert row[1].get_bound(Breakpoint.lg) == Bound(360.0, 360.0)
     assert row[1].get_bound(Breakpoint.xl) == Bound(427.5, 427.5)
+# Sample xs vw 572 : col3 + col + col: 143 + 214.5 + 214.5 = 572
 
 
 def test_xs_cols_with_auto_and_flex():
@@ -43,17 +44,18 @@ def test_xs_cols_with_auto_and_flex():
     assert row[0].get_bound(Breakpoint.lg) == Bound(240.0, 240.0)
     assert row[0].get_bound(Breakpoint.xl) == Bound(285.0, 285.0)
 
-    assert row[1].get_bound(Breakpoint.xs) == Bound(30.0, 369.0)
-    assert row[1].get_bound(Breakpoint.sm) == Bound(30.0, 345.0)
-    assert row[1].get_bound(Breakpoint.md) == Bound(30.0, 480.0)
-    assert row[1].get_bound(Breakpoint.lg) == Bound(30.0, 660.0)
-    assert row[1].get_bound(Breakpoint.xl) == Bound(30.0, 795.0)
+    assert row[1].get_bound(Breakpoint.xs) == Bound(30.0, 399.0)
+    assert row[1].get_bound(Breakpoint.sm) == Bound(30.0, 375.0)
+    assert row[1].get_bound(Breakpoint.md) == Bound(30.0, 510.0)
+    assert row[1].get_bound(Breakpoint.lg) == Bound(30.0, 690.0)
+    assert row[1].get_bound(Breakpoint.xl) == Bound(30.0, 825.0)
 
-    assert row[2].get_bound(Breakpoint.xs) == Bound(30.0, 369.0)
-    assert row[2].get_bound(Breakpoint.sm) == Bound(30.0, 345.0)
-    assert row[2].get_bound(Breakpoint.md) == Bound(30.0, 480.0)
-    assert row[2].get_bound(Breakpoint.lg) == Bound(30.0, 660.0)
-    assert row[2].get_bound(Breakpoint.xl) == Bound(30.0, 795.0)
+    assert row[2].get_bound(Breakpoint.xs) == Bound(30.0, 399.0)
+    assert row[2].get_bound(Breakpoint.sm) == Bound(30.0, 375.0)
+    assert row[2].get_bound(Breakpoint.md) == Bound(30.0, 510.0)
+    assert row[2].get_bound(Breakpoint.lg) == Bound(30.0, 690.0)
+    assert row[2].get_bound(Breakpoint.xl) == Bound(30.0, 825.0)
+# Sample xs vw 572 : col-3 + col-auto + col : 143 + 399 + 30 = 572
 
 
 def test_mix_flex_with_fixed():
@@ -101,20 +103,70 @@ def test_nicolas():
     row.add_column(Bootstrap4Column('col-auto'))
     row.add_column(Bootstrap4Column('col-2'))
     row.compute_column_bounds()
-    assert row[0].get_bound(Breakpoint.xs) == Bound(30.0, 416.7)
-    assert row[0].get_bound(Breakpoint.sm) == Bound(30.0, 390.0)
-    assert row[0].get_bound(Breakpoint.md) == Bound(30.0, 540.0)
-    assert row[0].get_bound(Breakpoint.lg) == Bound(30.0, 740.0)
-    assert row[0].get_bound(Breakpoint.xl) == Bound(30.0, 890.0)
+    assert row[0].get_bound(Breakpoint.xs) == Bound(30.0, 446.7) # still needs to round
+    assert row[0].get_bound(Breakpoint.sm) == Bound(30.0, 420.0)
+    assert row[0].get_bound(Breakpoint.md) == Bound(30.0, 570.0)
+    assert row[0].get_bound(Breakpoint.lg) == Bound(30.0, 770.0)
+    assert row[0].get_bound(Breakpoint.xl) == Bound(30.0, 920.0)
 
-    assert row[1].get_bound(Breakpoint.xs) == Bound(30.0, 416.7)
-    assert row[1].get_bound(Breakpoint.sm) == Bound(30.0, 390.0)
-    assert row[1].get_bound(Breakpoint.md) == Bound(30.0, 540.0)
-    assert row[1].get_bound(Breakpoint.lg) == Bound(30.0, 740.0)
-    assert row[1].get_bound(Breakpoint.xl) == Bound(30.0, 890.0)
+    assert row[1].get_bound(Breakpoint.xs) == Bound(30.0, 446.7)
+    assert row[1].get_bound(Breakpoint.sm) == Bound(30.0, 420.0)
+    assert row[1].get_bound(Breakpoint.md) == Bound(30.0, 570.0)
+    assert row[1].get_bound(Breakpoint.lg) == Bound(30.0, 770.0)
+    assert row[1].get_bound(Breakpoint.xl) == Bound(30.0, 920.0)
 
     assert row[2].get_bound(Breakpoint.xs) == Bound(53.3, 95.3)
     assert row[2].get_bound(Breakpoint.sm) == Bound(90.0, 90.0)
     assert row[2].get_bound(Breakpoint.md) == Bound(120.0, 120.0)
     assert row[2].get_bound(Breakpoint.lg) == Bound(160.0, 160.0)
     assert row[2].get_bound(Breakpoint.xl) == Bound(190.0, 190.0)
+# Sample vw 572px : col + col-auto + col-2 : 446.7 + 30 + 95.3 = 572
+
+
+def test4_cols_with_auto_and_fixed():
+    container = Bootstrap4Container()
+    row = container.add_row(Bootstrap4Row())
+    row.add_column(Bootstrap4Column('col-auto'))
+    row.add_column(Bootstrap4Column('col-auto'))
+    row.add_column(Bootstrap4Column('col-auto'))
+    row.add_column(Bootstrap4Column('col-5'))
+    row.compute_column_bounds()
+    assert row[0].get_bound(Breakpoint.xs) == Bound(30.0, 273.7)
+    assert row[0].get_bound(Breakpoint.sm) == Bound(30.0, 255.0)
+    assert row[0].get_bound(Breakpoint.md) == Bound(30.0, 360.0)
+    assert row[0].get_bound(Breakpoint.lg) == Bound(30.0, 500.0)
+    assert row[0].get_bound(Breakpoint.xl) == Bound(30.0, 605.0)
+
+    assert row[3].get_bound(Breakpoint.xs) == Bound(133.3, 238.3)
+    assert row[3].get_bound(Breakpoint.sm) == Bound(225.0, 225.0)
+    assert row[3].get_bound(Breakpoint.md) == Bound(300.0, 300.0)
+    assert row[3].get_bound(Breakpoint.lg) == Bound(400.0, 400.0)
+    assert row[3].get_bound(Breakpoint.xl) == Bound(475.0, 475.0)
+# Sample xl vw 1140 : col-auto + col-auto + col-auto + col-5: 605.0 + 30.0 + 30.0 + 475= 1140
+
+
+def test5_growing_columns():
+    container = Bootstrap4Container()
+    row = container.add_row(Bootstrap4Row())
+    row.add_column(Bootstrap4Column('col-12 col-sm-auto col-lg-4'))
+    row.add_column(Bootstrap4Column('col-12 col-sm-7 col-lg-4'))
+    row.add_column(Bootstrap4Column('col-12 col-sm-2 col-lg-4'))
+    row.compute_column_bounds()
+    assert row[0].get_bound(Breakpoint.xs) == Bound(320, 572.0)
+    assert row[0].get_bound(Breakpoint.sm) == Bound(30.0, 135.0)
+    assert row[0].get_bound(Breakpoint.md) == Bound(30.0, 180.0)
+    assert row[0].get_bound(Breakpoint.lg) == Bound(320.0, 320.0)
+    assert row[0].get_bound(Breakpoint.xl) == Bound(380.0, 380.0)
+
+    assert row[1].get_bound(Breakpoint.xs) == Bound(320.0, 572.0)
+    assert row[1].get_bound(Breakpoint.sm) == Bound(315.0, 315.0)
+    assert row[1].get_bound(Breakpoint.md) == Bound(420.0, 420.0)
+    assert row[1].get_bound(Breakpoint.lg) == Bound(320.0, 320.0)
+    assert row[1].get_bound(Breakpoint.xl) == Bound(380.0, 380.0)
+
+    assert row[2].get_bound(Breakpoint.xs) == Bound(320.0, 572.0)
+    assert row[2].get_bound(Breakpoint.sm) == Bound(90.0, 90.0)
+    assert row[2].get_bound(Breakpoint.md) == Bound(120.0, 120.0)
+    assert row[2].get_bound(Breakpoint.lg) == Bound(320.0, 320.0)
+    assert row[2].get_bound(Breakpoint.xl) == Bound(380.0, 380.0)
+# Sample sm vw 540 : col-sm-auto + col-sm-7 + col-sm-2 : 135.0 + 315.0 + 90 = 540
