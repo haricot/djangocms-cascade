d86ff7edcbf1a96c93d9e5a8d42ded91747f59b1
Nicolas PASCAL <np.pascal@gmail.com>
2019-10-11 23:01:02 +0200

    Update test_tabset.py

--- tests/bootstrap4/test_tabset.py
+++ tests/bootstrap4/test_tabset.py
@@ -44,7 +44,8 @@ def test_merge_extra_fields_tabset(rf, admin_site, bootstrap_tabset):
     assert 'extra_css_classes' in list(ModelForm.declared_fields)
     assert ('nav-tabs', 'nav-tabs') in list(ModelForm.declared_fields['extra_css_classes']._choices)
     if hasattr(ModelForm.declared_fields['custom_css_classes_and_styles'],'_choices'):
-        assert 'extra_inline_styles:border-radius' in list(ModelForm.base_fields['custom_css_classes_and_styles'])
+        assert 'extra_inline_styles:border-radius' in list(ModelForm.base_fields.__dict__)
+        assert 'extra_inline_styles:border-radius' in list(ModelForm.base_fields['custom_css_classes_and_styles']._choices)
     data = {
               "num_children": 1, 
               "hide_plugin": False,
