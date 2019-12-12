596a71021cb03e315e0bf4951b1f53c32a5ae1de
Haricot <np.pascal@gmail.com>
2019-12-12 20:51:52 +0100

    wip compact ui

--- cmsplugin_cascade/fields.py
+++ cmsplugin_cascade/fields.py
@@ -302,30 +302,37 @@ class CascadeEntangledBoundField(EntangledBoundField):
     label = '' #prevent tag and title label
     auto_id = False # prevent tag label
     template_name = 'cascade/admin/widgets/boundfield_as_widget.html'
-    
+
     def __init__(self, *args,**kwargs):
+        cls_cascade_entangled_form_field=args[1]
+        self.title = cls_cascade_entangled_form_field.title
+        self.icon =  cls_cascade_entangled_form_field.icon
         self.widget = EntangledFormWidget(self)
         super().__init__(*args,**kwargs)
         
-    def as_widget(self, widget=None, attrs=None, only_initial=True ):
-
+    def as_widget(self, widget=None, attrs=None, only_initial=True):
         output_widgets=EntangledBoundField.as_widget(self)
-        
         context ={
         "help_text": mark_safe(self.widget._entangled_form.help_text),
         'output_widgets':output_widgets,
-        'title_label':self.widget._entangled_form.name
+        #'label':self.widget._entangled_form.name, self.icon
+        'title':self.title,
+        'icon' :self.icon
         }
         template = loader.get_template(self.template_name).render(context)
         return mark_safe(template)
 
 
-class CascadeEntangledFormField(EntangledFormField ):
+class CascadeEntangledFormField(EntangledFormField,  ):
     _html_output_kwargs= dict(   normal_row='<li%(html_class_attr)s>%(errors)s%(label)s %(field)s%(help_text)s</li>',
             error_row='<li>%s</li>',
             row_ender='</li>',
             help_text_html=' <span class="helptext">%s</span>',
             errors_on_separate_row=False)
+    def __init__(self, *args,**kwargs):
+        self.title = args[0].title if hasattr(args[0], 'title') else ''
+        self.icon = args[0].title if hasattr(args[0], 'icon') else ''
+        super().__init__(*args,**kwargs)
 
     def get_bound_field(self, form, field_name):
         return CascadeEntangledBoundField(form, self, field_name)
